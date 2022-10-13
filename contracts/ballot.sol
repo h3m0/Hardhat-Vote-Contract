7f9c8f2f6dbe9e27470d20f843f8d24f3fb7a184
smarsbi <93432045+h3m0@users.noreply.github.com>
2022-10-13 13:02:05 +0100

    Update ballot.sol

--- contracts/ballot.sol
+++ contracts/ballot.sol
@@ -1,143 +1,110 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
-// 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-// 1440000000000000000
 
 
-contract Ballot is VRFConsumerBase{ 	
+contract Ballot{ 	
 
-		address payable public currentLeader;
-		AggregatorV3Interface internal priceFeed;
-		uint256 internal Ccount;
-		uint256 internal Vcount;
-		bytes32 keyhash;
-		uint256 fee;	
-		uint256 internal randomResult;
-		Candidate[] public candidates;
-		Voter[] public voters; 
-		uint256 public fee;	
-		mapping(address => bytes32) public voterToId;		
-		mapping(address => Voter) public voteraddrtoData;
-		mapping(address => Candidate) public candidateaddrtoData;
-		mapping(address => address) public voteraddrToCandidateaddr;	
-		mapping(address => Candidate) public voterToCandidate;
+	address payable public currentLeader;
+	uint256 internal Ccount;
+	uint256 internal Vcount;
+	Candidate[] public candidates;
+	Voter[] public voters;
+	uint256 internal immutable Efee;
+	mapping(address => uint256) public voterToId;		
+	mapping(address => Voter) public voteraddrtoData;
+	mapping(address => Candidate) public candidateaddrtoData;
+	mapping(address => address) public voteraddrToCandidateaddr;	
+	mapping(address => Candidate) public voterToCandidate;
 
-	modifier onlyCurrentleader{
-		require(msg.sender == currentLeader,"Only the current leader can call this function!");
+	modifier onlyCurrentLeader{
+		require(msg.sender == currentLeader, "Only the current leader can call this function!");
 		_;
 	}
 
-	constructor(
-		address _addr,
-		address _link,
-		address _vrf,
-	    bytes32 _keyhash,
-	    uint256 fee,
-	) VRFConsumerBase(_vrf,,_link){
-		currentLeader = payable(msg.sender);
-		priceFeed = AggregatorV3Interface(_addr);
-		Efee = 1000000;
-		fee = _fee;
-		keyhash = _keyhash;
+	enum State { OPEN, CLOSED }
+	State internal currentState; 
+
+	constructor() {
+		currentLeader = payable(msg.sender);		
+		Efee = 1 ether;
+		currentState = State.CLOSED;
 	}
 
 	struct Voter{
 		string name;
 		uint age;
-		bool isCitizen;	
-		uint yearsInJail;	 
 		bool hasVoted;
 		uint count;	
 		address Vaddress;
-		uint256 count2;
+		bool isVoter;
 	}
 
 	struct Candidate{
-		string name;
-		uint yearsInJail;
-		uint age;
-		bool isCitizen;	
+		string name;		
+		uint age;		
 		uint256 votes;
 		uint256 count;
 		address Caddress;
-		uint256 count2;
+		bool isCandidate;
 	}		
 
-	// function ETHtoNAIRA(uint256 _eth) public view returns(uint256){
-	// 	  (,int256 answer,,,) = priceFeed.latestRoundData();    	  
-	// 	  uint256 convertedeth = _eth * (uint256(answer) / 10 ** 8) * 600;
-	// 	  return convertedeth;
-	// }
-
 	function becomeCandidate(
-		string memory _name,
-		uint256 _yearsInJail,
-		uint256 _age, 
-		bool _isCitizen
-	) payable public returns(uint256){
-		// require(ETHtoNAIRA(msg.value) >= Efee, "You need to spend more ETH");
-		require(candidateaddrtoData[msg.sender].count2 < 1, "You are already a candidate");
-		require(_yearsInJail <= 3, "Sorry, you have spent too many years in jail");
-		require(_isCitizen == true, "Only citizens can be leader");
+		string memory _name,		
+		uint256 _age 		
+	) payable public returns(string memory){
+		require(msg.value >= Efee, "You need to spend more ETH");
+		require(candidateaddrtoData[msg.sender].isCandidate != true, "You are already a candidate");
 		require(_age >= 35, "You are too young to be the leader");
 		Candidate memory aspirant = Candidate(
 			{
-				name: _name,
-			    yearsInJail: _yearsInJail,
-			    age: _age,
-			    isCitizen: _isCitizen,		   
+				name: _name,			   
+			    age: _age,			    	   
 			    votes: 0,
 			    count: Ccount,
-			    count2: 1,
+			    isCandidate: true,
 			    Caddress: msg.sender
 			}
 		);
-		currentLeader.transfer(msg.value);
+		address payable This = payable(address(this));
+		This.transfer(msg.value);
 		candidates.push(aspirant);		
 		candidateaddrtoData[msg.sender] = aspirant;			
-		Ccount++;	
-		return Ccount;
+		Ccount++;
+		return string(abi.encodePacked("Your Candidate Number is: ", Ccount));			
 	}
 
-	function register(
-		string memory _name,
-		uint256 _yearsInJail,
-		uint256 _age, 
-		bool _isCitizen
-	) public returns(uint256, bytes32){		
+	function register(		
+		string memory _name,		
+		uint256 _age,
+		uint256 _ID 		
+	) public returns(string memory){		
 		require(msg.sender != currentLeader, "The current leader cannot vote");
-		require(voteraddrtoData[msg.sender].count2 < 1, "You have already registered");
-		require(_yearsInJail <= 3, "Sorry, you have spent too many years in jail");
-		require(_isCitizen == true, "Only citizens can vote");
-		require(_age >= 18, "You are too young to vote");
+		require(voteraddrtoData[msg.sender].isVoter != true, "You have already registered");
+		require(_age >= 18, "You are too young to vote");		
 		Voter memory voter = Voter(
 			{
-				name: _name,
-				yearsInJail: _yearsInJail,
-				age: _age, 
-				isCitizen: _isCitizen,
+				name: _name,				
+				age: _age, 				
 				hasVoted: false,			
 				count: Vcount,
-				count2: 1,
+				isVoter: true,
 				Vaddress: msg.sender		
 			}
 		);	  
 		voters.push(voter);	
-		voteraddrtoData[msg.sender] = voter;
-		requestRandomness(keyhash, fee);
-		voterToId[msg.sender] = randomResult;
+		voteraddrtoData[msg.sender] = voter;		
+		voterToId[msg.sender] = _ID;
 		Vcount++;
-		return (Vcount, randomResult);
+		return string(abi.encodePacked("Your Voter's No is: ", Vcount));
 	}		
 
 	function vote(
 		uint256 _vindex, 
 		uint256 _index, 
-		bytes32 _Id
+		uint256 _Id
 	) public{	
 		require(_index <= Ccount, "Invalid Candidate number!");
+		require(currentState == State.OPEN, "Can't vote yet");
 		require(_Id == voterToId[msg.sender], "Invalid Id");
 		require(_vindex <= Vcount, "Invalid Voter number!");	
 		require(voters[_vindex - 1].hasVoted != true, "You have already voted");			
@@ -147,39 +114,26 @@ contract Ballot is VRFConsumerBase{
 		voterToCandidate[msg.sender] = candidates[_index - 1];
 	}
 
-	function fulfillRandomness(bytes32 _requestId, uint256 _randomness) internal virtual override{
-		randomResult = _randomness;
-	}
-
-	 function checkWinner() public onlyCurrentleader returns(uint){
+	 function checkWinner() view public returns(uint){
 	 	uint king;
-	 	uint winningP;
+	 	uint winningP;	 	
 	 	for(uint i = 0; i < candidates.length; i++){	 		
 	 		if(candidates[i].votes > king){
 	 			king = candidates[i].votes;	
 	 			winningP = i;
 	 		}	    
 	 	}	 	
-	 	return winningP
+	 	return winningP;
 	 }
 
-	 function revealWinner() return(string memory){
-	 	return candidates[checkWinner()].name;
+	 function startElection() onlyCurrentLeader public {
+	 	currentState = State.OPEN;
 	 }
+	 
+
+	function endElection() public onlyCurrentLeader returns(string memory) {
+		currentState = State.CLOSED;
+		currentLeader = payable(candidates[checkWinner()].Caddress);
+		return string(abi.encodePacked(candidates[checkWinner()].name, " won the election!"));		
+	}
 }
-
-
-// I want to share with you all a smart contract that allows you to book and leave hotel rooms. It is a modified version of the Dapp University solidity tutorial that i thought  of to really challenge myself.  It allows users to systematically book and leave room. There is also a new hotel function that allows you to add a new custom hotel room. I plan to make this a full web app when I finish learning on Mr Patrick's blockchain dev(java script) course. I am open to criticism and corrections. Special thanks to Dapp University, Mr Patrick Collins and  freeCodeCamp. 
-
-// freecodecamp
-
-//  https://www.youtube.com/watch?v=gyMwXuJrbJQ&t=19294s
-
-// Dapp university 
-
-// https://www.youtube.com/watch?v=nORC_s2HzAg
-
-
-
-// #freecodecamp #DappUniversity #Solidiy #Smartcontracts #ethereumblockchain #javascript #share #blockchain #learning 
-
